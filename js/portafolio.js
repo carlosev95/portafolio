db7284e9d65bbc1b24b2b624a3bae2b2597b70b5
Carlos E <cromo_lp@hotmail.com>
2023-07-08 00:45:51 -0600

    Modificamos el contenido de los section para mejorar su diseño

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,1 +1,10 @@
-const name = "Carlos Escobar Vázquez"
+let name = "Carlos Escobar Vázquez"
+//Insertamos nuestro nombre en el DOM
+document.getElementById("name").textContent = name
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+});
